30ab86b9a54ebc6f36572a7816021cb4cc2214b4
noariuchiha <WAIRECRAFFTERLOUANNE2020>
2021-04-11 22:43:38 +0200

    Accueil.vue in progress

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,12 @@
   <router-view/>
 </template>
 
-<style scoped>
-
+<style>
+@font-face {
+  font-family: "Raleway-Regular";
+  src: url("assets/fonts/Raleway-VariableFont_wght.ttf") format("truetype");
+}
+#app{
+  font-family: Raleway-Regular,sans-serif !important;
+}
 </style>
