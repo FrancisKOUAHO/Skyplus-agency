c2d59ae4b8f08fedf82099728d935999563c4ae5
noariuchiha <WAIRECRAFFTERLOUANNE2020>
2021-04-14 01:33:40 +0200

    Accueil.vue in progress

--- src/components/LineVertical.vue
+++ src/components/LineVertical.vue
@@ -17,7 +17,7 @@ export default defineComponent({
 <style scoped>
 .hr-vertical {
   border: 0;
-  border-left: 3px solid white;
+  border-left: 1px solid white;
   height: 220px;
   margin-left: 35%;
   margin-top: -9%;
