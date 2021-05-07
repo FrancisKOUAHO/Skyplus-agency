8243a0b863e24c1c096084b75f435abb0cdcb644
noariuchiha <WAIRECRAFFTERLOUANNE2020>
2021-05-08 01:59:24 +0200

    stripe ok

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,8 @@ import Calendrier from '@/views/Admin/Calendrier.vue'
 import Projet from '@/views/Admin/Projet.vue'
 import Facturation from '@/views/Admin/Facturation/Facturation.vue'
 import EditerOffres from '@/views/Admin/EditerOffres/EditerOffres.vue'
+import SuccessPayment from '@/views/Offres/SuccessPayment.vue'
+import Canceled from '@/views/Offres/Canceled.vue'
 
 const routes: Array<RouteRecordRaw> = [
     {
@@ -45,6 +47,18 @@ const routes: Array<RouteRecordRaw> = [
         name: 'Offre',
         component: Offre,
         meta: {title: 'Skyplus |  Nos offres '}
+    },
+    {
+        path: '/success?session_id=',
+        name: 'SuccessPayment',
+        component: SuccessPayment,
+        meta: {title: 'Skyplus |  Succès Paiement '}
+    }
+    ,{
+        path: '/payement-refuser',
+        name: 'Canceled',
+        component: Canceled,
+        meta: {title: 'Skyplus |  Annulé '}
     },
     {
         path: '/editer-offres',
