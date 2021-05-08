d26b09e1adf98d5c06266d7435d7c39b7f6cbec1
noariuchiha <WAIRECRAFFTERLOUANNE2020>
2021-05-08 03:50:51 +0200

    stripe ok

--- src/router/index.ts
+++ src/router/index.ts
@@ -49,7 +49,7 @@ const routes: Array<RouteRecordRaw> = [
         meta: {title: 'Skyplus |  Nos offres '}
     },
     {
-        path: '/paiement-reussi',
+        path: '/paiement-reussi?session_id=',
         name: 'PaymentSuccessful',
         component: PaymentSuccessful,
         meta: {title: 'Skyplus |  Paiement réussi '}
